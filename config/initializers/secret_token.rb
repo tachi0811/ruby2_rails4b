d7e9ac092b3a09d82c7c27d233aafbfff7cfb94c
tachikawa <tachikawa@cybergrow.co.jp>
2013-06-25 16:39:44 +0900

    first-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myrails::Application.config.secret_key_base = '08004d5a5d5ddba6136de2fc022d3f7696816cd3a539cf6136556545dab4542a7c17055e878950b48f7a3e127a91c80a2f020c2402408a1a4897c9a0235b1600'
