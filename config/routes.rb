482012614bdce79a84941ca32489ef10a735bf22
tachikawa <tachikawa@cybergrow.co.jp>
2013-06-26 12:56:17 +0900

    login_form_add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Myrails::Application.routes.draw do
+  resources :posts
+
+  resources :sessions do
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
